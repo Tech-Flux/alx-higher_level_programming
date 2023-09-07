bbae79f5edaa4cd027888f9301b3e04bf8301460
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-09-07 23:50:15 +0300

    commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == '__name__':
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add(a, b):
+    print(a + b)
+
+add(3, 5)
